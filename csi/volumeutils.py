575f58c77f2ebb62c82ee609bc92203189f613d2
Amar Tumballi <amar@kadalu.io>
2021-01-27 18:33:12 +0530

    fix pylint warnings
    Signed-off-by: Amar Tumballi <amar@kadalu.io>

--- csi/volumeutils.py
+++ csi/volumeutils.py
@@ -353,8 +353,16 @@ def create_subdir_volume(hostvol_mnt, volname, size):
 
     #setfattr -n trusted.glusterfs.namespace -v true
     #setfattr -n trusted.gfs.squota.limit -v size
-    retry_errors(os.setxattr, [os.path.join(hostvol_mnt, volpath), "trusted.glusterfs.namespace", "true"], [ENOTCONN])
-    retry_errors(os.setxattr, [os.path.join(hostvol_mnt, volpath), "trusted.gfs.squota.limit", size], [ENOTCONN])
+    retry_errors(os.setxattr,
+                 [os.path.join(hostvol_mnt, volpath),
+                  "trusted.glusterfs.namespace",
+                  "true"],
+                 [ENOTCONN])
+    retry_errors(os.setxattr,
+                 [os.path.join(hostvol_mnt, volpath),
+                  "trusted.gfs.squota.limit",
+                  size],
+                 [ENOTCONN])
     count = 0
     while True:
         count += 1
@@ -454,7 +462,11 @@ def update_subdir_volume(hostvol_mnt, volname, expansion_requested_pvsize):
         pvsize_buffer=pvsize_buffer,
     ))
 
-    retry_errors(os.setxattr, [os.path.join(hostvol_mnt, volpath), "trusted.gfs.squota.limit", expansion_requested_pvsize], [ENOTCONN])
+    retry_errors(os.setxattr,
+                 [os.path.join(hostvol_mnt, volpath),
+                  "trusted.gfs.squota.limit",
+                  expansion_requested_pvsize],
+                 [ENOTCONN])
     count = 0
     while True:
         count += 1
